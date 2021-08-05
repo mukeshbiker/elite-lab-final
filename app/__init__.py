32c5f5d69ca94eabba741ee4e3c84508bb66625d
Kevin Koh <kevin.koh.dev@gmail.com>
2021-08-04 20:36:21 -0700

    fixed sqlalchemy 1.4 issue that sometimes breaks for some os

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import re
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -11,6 +12,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 database_url = os.environ.get('DATABASE_URL', None)
 
 if database_url:
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
@@ -24,3 +27,4 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import models, controllers
+
