6a46a3b2fe59f6d494ac233a366ac4d04de83eac
Mukesh Saravanan 2 <Mukesh-School@Mac-mini.local>
2022-04-20 19:25:02 -0700

    prep for deployment

--- app/models.py
+++ app/models.py
@@ -95,6 +95,8 @@ class Chat(db.Model):
 
     # Fill in your model here
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    hash_key = db.Column(db.String(10), index=True)
 
     # This represents the other side of the many-to-one relationship
     # This is not defined in the database, so don't worry about this
